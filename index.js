dd9708bf405702899948a1a94e5e0dd65315dc5e
github username <github email address>
2020-02-22 17:07:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const inputField = document.querySelector("input#input")
+    inputField.addEventListener("click", (event) => alert("you clicked me lol."))
 }
+
+addingEventListener()
